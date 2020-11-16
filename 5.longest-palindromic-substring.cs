bb62dbbd756bddeb0e8d8095ac86bb7d9dfa55bb
Wei Yan <im.wayne.wy@gmail.com>
2020-11-16 08:43:08 -0800

    5.longest-palindromic-substring

---
+++ 5.longest-palindromic-substring.cs
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=5 lang=csharp
+ *
+ * [5] Longest Palindromic Substring
+ */
+
+// @lc code=start
+public class Solution {
+    public string LongestPalindrome(string s) {
+        int maxLength = 0, startIndex = 0;
+        for (int i = 0; i<s.Length; i++) {
+            int start = i, end = i;
+            while(end < s.Length-1 && s[start] == s[end+1])
+                end++;
+            while(end < s.Length-1 && start>0 && s[start-1] == s[end+1]) {
+                start--;
+                end++;
+            }
+            if (maxLength < end - start + 1) {
+                maxLength = end - start + 1;
+                startIndex = start;
+            }
+        }
+        return s.Substring(startIndex, maxLength);
+    }
+}
+// @lc code=end
+
+/*
+    从中间向两边查找，记录start和end位置
+    13行的作用是用于查找类似于abba这样的偶数对称字符串
+*/
