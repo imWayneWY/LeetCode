57d3d443eaa8c1890dcd03aaa91367ee3bed0c46
Wei Yan <im.wayne.wy@gmail.com>
2020-11-21 18:57:34 -0800

    48.rotate-image

--- 48.rotate-image.cs
+++ 48.rotate-image.cs
@@ -7,6 +7,16 @@
 // @lc code=start
 public class Solution {
     public void Rotate(int[][] matrix) {
+        int n = matrix.Length;
+        for (int b = 0; b < n/2; b++) {
+            for (int i = b; i < (n-1)-b; i++) {
+                int tl = matrix[b][i];
+                matrix[b][i] = matrix[(n-1)-i][b];
+                matrix[(n-1)-i][b] = matrix[(n-1)-b][(n-1)-i];
+                matrix[(n-1)-b][(n-1)-i] = matrix[i][(n-1)-b];
+                matrix[i][(n-1)-b] = tl;
+            }
+        }
     }
 }
 // @lc code=end
