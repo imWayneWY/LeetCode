888405ac1d7fc79541b03c240c8e29e88ae50785
Wei Yan <im.wayne.wy@gmail.com>
2020-12-30 09:38:58 -0800

    35.search-insert-position

---
+++ 35.search-insert-position.cs
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=35 lang=csharp
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        int i = 0;
+        int j = nums.Length-1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (target < nums[mid])
+                j = mid - 1;
+            else if (target > nums[mid])
+                i = mid + 1;
+            else
+                return mid;
+        }
+        return i;
+    }
+}
+// @lc code=end
+
