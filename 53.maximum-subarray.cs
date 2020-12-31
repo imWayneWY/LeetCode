8f36e62df7579779c8422de0f7742959ddc37600
Wei Yan <im.wayne.wy@gmail.com>
2020-12-30 18:07:21 -0800

    53.maximun-subarray.cs

---
+++ 53.maximum-subarray.cs
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=53 lang=csharp
+ *
+ * [53] Maximum Subarray
+ */
+
+// @lc code=start
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        int sum = 0;
+        int maxSum = nums[0];
+
+        for (int i=0; i<nums.Length; i++) {
+            sum += nums[i];
+            if (nums[i] > sum) {
+                sum = nums[i];
+            }
+            if (sum > maxSum) { 
+                maxSum = sum; 
+            }
+        }
+        return maxSum;
+    }
+}
+// @lc code=end
+
